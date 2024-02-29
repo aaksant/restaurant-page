ce5e4310ec759dfa2abe7130ed22aa1dde57774a
aaksant <aaksangit@gmail.com>
2024-02-29 23:02:00 +0700

    feat: add load home menu initial logic

--- src/page.js
+++ src/page.js
@@ -1,10 +1,65 @@
-import loadHeader from "./header";
-import loadFooter from "./footer";
+import loadHome from './home';
 
-function loadPage() {
-  const body = document.querySelector('body');
-  body.appendChild(loadHeader());
-  body.appendChild(loadFooter());
+function loadHeader() {
+  function createButton(text) {
+    const li = document.createElement('li');
+    const tab = document.createElement('button');
+
+    tab.classList.add('nav-btn');
+    tab.textContent = text;
+
+    li.appendChild(tab);
+
+    return li;
+  }
+
+  const header = document.createElement('header');
+  const logoContainer = document.createElement('div');
+  const nav = document.createElement('nav');
+  const navItemContainer = document.createElement('ul');
+
+  header.classList.add('header');
+  logoContainer.classList.add('logo-container');
+  nav.classList.add('nav-container');
+  navItemContainer.classList.add('nav-item-container');
+
+  logoContainer.textContent = 'noodlenirvana';
+
+  const homeBtn = createButton('Home');
+  const menuBtn = createButton('Menu');
+  const aboutBtn = createButton('About');
+
+  navItemContainer.appendChild(homeBtn);
+  navItemContainer.appendChild(menuBtn);
+  navItemContainer.appendChild(aboutBtn);
+
+  nav.appendChild(navItemContainer);
+
+  header.appendChild(logoContainer);
+  header.appendChild(nav);
+
+  return header;
 }
 
-export default loadPage;
+function loadFooter() {
+  const footer = document.createElement('footer');
+  const link = document.createElement('a');
+
+  footer.classList.add('footer');
+  footer.textContent = 'Made by ';
+  link.href = 'https://github.com/aaksant/restaurant-page';
+  link.textContent = 'aaksant';
+
+  footer.appendChild(link);
+
+  return footer;
+}
+
+export default function loadPage() {
+  const body = document.querySelector('body');
+  const content = document.getElementById('content');
+
+  body.appendChild(loadHeader());
+  content.appendChild(loadHome());
+  body.appendChild(loadFooter());
+}
